4ed5b6676f89ad21389547cb68a799532770d18d
Richard Liu <richy.liu.2002@gmail.com>
2017-03-11 21:04:57 -0800

    US Open 9:04pm

--- mar11silver/cow1.py
+++ mar11silver/cow1.py
@@ -31,6 +31,34 @@ def sort(array):
         return array
 
 
+def select(L):
+    if len(L) < 10:
+        L.sort()
+        return L[int(len(L)/2)]
+    S = []
+    lIndex = 0
+    while lIndex+5 < len(L)-1:
+        S.append(L[lIndex:lIndex+5])
+        lIndex += 5
+    S.append(L[lIndex:])
+    Meds = []
+    for subList in S:
+        Meds.append(select(subList))
+    L2 = select(Meds)
+    L1 = L3 = []
+    for i in L:
+        if i < L2:
+            L1.append(i)
+        if i > L2:
+            L3.append(i)
+    if len(L) < len(L1):
+        return select(L1)
+    elif len(L) > len(L1) + 1:
+        return select(L3)
+    else:
+        return L2
+
+
 
 
 lines = read('pairup')
@@ -42,16 +70,21 @@ cows = []
 maxTime = 0 # minimize this
 
 
-
+smallest = 1e9
+largest = 0
 for i in range(1, dataLength + 1):
-    cur = int(lines[i].split(' ')[1])
-    cows += [cur] * int(lines[i].split(' ')[0])
-pairs = len(cows)
-cows = sort(cows)
+    curLine = lines[i].split(' ')
+    cur = int(curLine[1])
+    cows += [cur] * int(curLine[0])
+    
+    if cur < smallest:
+        smallest = cur
+    if cur > largest:
+        largest = cur
 
 
 # longest time could be center two or first and last
-maxTime = max(cows[pairs // 2 - 1] + cows[pairs // 2], cows[0] + cows[pairs - 1])
+maxTime = max(select(cows) + select(cows[:-1]), cows[0] + cows[len(cows) - 1])
 
 
 print(maxTime)
